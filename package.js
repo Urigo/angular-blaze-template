b8fc9b932aaa20b28c162ed6804a5840b5c9fe5c
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-11-10 00:23:42 +0200

    Release 0.2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'urigo:angular-blaze-template',
-  version: '0.1.2',
+  version: '0.2.0',
   summary: 'Include Blaze templates in your angular-meteor application.',
   git: 'https://github.com/Urigo/angular-blaze-template',
   documentation: 'README.md'
